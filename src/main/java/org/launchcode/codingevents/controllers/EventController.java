dfe4e7b48d4baf4e137571a476f34a0e5ed42493
Edward King <king8three@gmailcom>
2024-10-19 15:12:00 -0400

    catching up, models

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,10 +4,7 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,37 +15,41 @@ public class EventController {
 
 
     @GetMapping
-    public String displayAllEvents(Model model){
-       model.addAttribute("events", EventData.getAll());
+    public String displayAllEvents(Model model) {
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
 
     }
+
     @GetMapping("create")
-    public String renderCreateEventForm () {
+    public String renderCreateEventForm() {
 
         return "events/create";
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription){
-        EventData.add(new Event(eventName, eventDescription));
+    public String createEvent(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
         return "redirect:/events";
 
 
     }
+
     @GetMapping("delete")
-    public String displayDeleteEventForm(Model model){
+    public String displayDeleteEventForm(Model model) {
         model.addAttribute("title", "Delete Events");
         model.addAttribute("events", EventData.getAll());
         return "events/delete";
     }
 
     @PostMapping("delete")
-    public String processDeleteEventsForm(@RequestParam int[] eventIds){
-        for (int id : eventIds){
-            EventData.remove(id);
+    public String processDeleteEventsForm(@RequestParam int[] eventIds) {
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
         }
         return "redirect:/events";
-
     }
+
 }
